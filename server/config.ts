1c691dbd9dc2fbb656d4589d953cbf358b1cf6aa
IvanZemsky <i.ivanzemsky2005@gmail.com>
2026-01-04 23:32:43 +0300

    fix: try new readJsonFile function

--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,15 @@
+import { resolve } from "path"
+
 const DATA_PATH = "server/data"
 
 export function getDataFileName(...strings: string[]) {
-    return [DATA_PATH, ...strings].join('/') + ".json"
+  return [DATA_PATH, ...strings].join("/") + ".json"
+}
+
+export async function loadDataFile(...strings: string[]) {
+  const fileName = strings.join("/")
+
+  const module = await import(`~~/server/data/${fileName}.json`)
+  console.log(module)
+  return module.default || module
 }
--- server/utils/json.ts
+++ server/utils/json.ts
@@ -1,8 +1,29 @@
+// utils/fs-utils.ts
 import { readFile } from "fs/promises"
 import { resolve } from "path"
 
 export async function readJsonFile<T>(relativePath: string): Promise<T> {
-  const rootDir = process.cwd()
-  const fullPath = resolve(rootDir, relativePath)
-  return JSON.parse(await readFile(fullPath, "utf-8"))
+  try {
+    if (process.env.NODE_ENV !== "production") {
+      const rootDir = process.cwd()
+      const fullPath = resolve(rootDir, relativePath)
+      return JSON.parse(await readFile(fullPath, "utf-8"))
+    }
+
+    const fullPath = resolve(
+      process.cwd(),
+      ".vercel/output/functions",
+      relativePath
+    )
+
+    try {
+      return JSON.parse(await readFile(fullPath, "utf-8"))
+    } catch {
+      const module = await import(relativePath)
+      return module.default || module
+    }
+  } catch (error) {
+    console.error("Error reading JSON file:", error)
+    throw error
+  }
 }
